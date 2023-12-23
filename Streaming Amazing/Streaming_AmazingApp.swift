b39708025548969d11d2c6f2ec804e81d271539e
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2023-12-23 10:04:40 -0300

    Initial Commit

---
+++ Streaming Amazing/Streaming_AmazingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Streaming_AmazingApp.swift
+//  Streaming Amazing
+//
+//  Created by kenjimaeda on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Streaming_AmazingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
